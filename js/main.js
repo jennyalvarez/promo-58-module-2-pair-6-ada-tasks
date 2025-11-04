55881745c0d0e983a746a5c1cce65b0659beda7f
Sus.diaz <sus.diazse@gmail.com>
2025-11-04 17:42:12 +0100

    complete the request to the server and add possible error

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// -------------------------------------array ---------------------------------------------------
 /*const tasks = [
   { name: "Recoger setas en el campo", completed: true, id: 1 },
   { name: "Comprar pilas", completed: true, id: 2 },
@@ -8,15 +9,18 @@
     id: 4,
   },
 ];
+
+// -----------------------------------------constantes --------------------------------------------------
 const taskListUl = document.querySelector(".js_task_list");
 const taskInput = document.querySelector(".js_task_input");
 const taskButton = document.querySelector(".js_task_button");
 
-function renderTask() {
+// -------------------------------------- función ---------------------------------------------------
+function renderTask() { // 
 
-taskListUl.innerHTML = "";
+taskListUl.innerHTML = ""; // 
 
-for (const task of tasks) {
+for (const task of tasks) {  
   if (task.completed === true) {
     taskListUl.innerHTML += `
 <li class="completed">
@@ -35,6 +39,9 @@ for (const task of tasks) {
 }
   }
 */
+
+// -----------------------------------------constantes----------------------------------------
+const taskListUl = document.querySelector(".js_task_list");
 
 const gitHub_user = `susdiaz-sudo`;
 const server_url = `https://dev.adalab.es/api/todo/${gitHub_user}`;
@@ -42,38 +49,39 @@ const server_url = `https://dev.adalab.es/api/todo/${gitHub_user}`;
 const gitHub_user_2 = `jennyalvarez`;
 const server_url_2 = `https://dev.adalab.es/api/todo/${gitHub_user_2}`;
 
+// ---------------------------------------------petición al server------------------------------------------
+
 fetch(server_url)
   .then((response) => response.json())
-  .then(data => {
-    console.log(data.info);
-    console.log(data.results);
-    tasks = data.results;
+  .then((data) => {
+    console.log(data.info); // información de la respuesta a la petición con el contenido que incluye la url
+    console.log(data.results); // información de las tareas de la url
+    tasks = data.results; // tareas
 
-    function renderTask() {
+    function renderTask() { // función para pintar el contenido de las tareas
+      taskListUl.innerHTML = ""; // limpia el contenido del elemento "lista de tareas"
 
-taskListUl.innerHTML = "";
-
-for (const task of tasks) {
-  if (task.completed === true) {
-    taskListUl.innerHTML += `
-<li class="completed">
-<input type="checkbox" name="${task.name}" id="${task.id}" checked />
-<label for="${task.id}">${task.name}</label>
-</li>
-`;
-  } else {
-    taskListUl.innerHTML += `
-<li>
-<input type="checkbox" name="${task.name}" id="${task.id}" />
-<label for="${task.id}">${task.name}</label>
-</li>
-`;
-  }
-}
-  }
-    renderTask();
+      for (const task of tasks) { // para cada tarea de la lista de tareas
+        if (task.completed === true) { // si la tarea está completada (true), pinta la tarea con la clase "completed" para tachar la tarea y "checked" para que aparezca el tic. 
+          taskListUl.innerHTML += `   
+      <li class="completed">
+      <input type="checkbox" name="${task.name}" id="${task.id}" checked />
+      <label for="${task.id}">${task.name}</label>
+      </li> 
+      `;
+        } else {   // si la tarea no está completada (false), pinta la tarea sin la clase y sin el tic.
+          taskListUl.innerHTML += `
+      <li>
+      <input type="checkbox" name="${task.name}" id="${task.id}" />
+      <label for="${task.id}">${task.name}</label>
+      </li>
+      `;
+        }
+      }
+    }
+    renderTask();  // llamada a la función
   })
-/*
-.catch((error) => console.error("Error al obtener tareas:", error));
-console.log("Error al obtener tareas:", error);
-*/
+
+  .catch((error) => {
+    console.error("Error al obtener tareas:", error);
+  });
